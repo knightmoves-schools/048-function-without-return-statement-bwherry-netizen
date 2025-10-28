006789c98beb2231f0d21d56299d4d3cd75ded3f
Brendon W. <bwherry@midland.k12.ia.us>
2025-10-28 12:25:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 
-function logUser(&{username}){
+function logUser(${username}){
   console.log(${username});
 }
 
 document.getElementById("result")innerHTML = logUser(bwherry-netizen);
+
 
 
 
